bcbb21b5a19b1ef55b321908a8430496da6bea4c
Ande Yashwanth <AndeYashwanth@users.noreply.github.com>
2021-05-16 18:16:40 +0530

    modified react router and fixed view documents click propogation issue

--- Admin System/client/src/views/users/RegisterdUsers.js
+++ Admin System/client/src/views/users/RegisterdUsers.js
@@ -107,7 +107,12 @@ const RegisterdUsers = () => {
                 'Documents':
                 (item)=>(
                   <td>
-                    <CButton component="a" color="primary" href={`/#/registerd/documents/user/${item.id}`} role="button">View</CButton>
+                    <CButton component="a" color="primary" 
+                      onClick={(e) => {
+                        history.push(`/registerd/documents/user/${item.id}`);
+                        e.stopPropagation();
+                      }} 
+                      role="button">View</CButton>
                   </td>  
                 )
             }}
